487670cd812996e4aacfa67232b961307e0e5d32
AnastasiaBatova <batosha9913@icloud.com>
2024-12-01 23:37:40 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = tuple ([1,'december',2024])
+print(immutable_var)
+immutable_var = tuple ([[1,'december',2024]])
+print(immutable_var)
+immutable_var[0][1]='Jule'
+print(immutable_var)
+mutable_list=['pen', 'coffe', 'film']
+print(mutable_list)
+mutable_list.append('cat')
+print(mutable_list)
